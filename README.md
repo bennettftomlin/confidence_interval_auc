0c5a16d2aff6ee36c1f18b01855a12eba909cc7c
Bennett Tomlin <bennettftomlin@gmail.com>
2019-08-08 18:58:47 -0500

    Initial Commit
    A simple module that can be used to calculate the confidence interval of an auc in Python using bootstrapping.

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # confidence_interval_auc
+
+Place script in working directory and then you can use function by:
+
+from conf_auc import conf_auc
+
+Pass the function: test_predictions, ground truth, and optionally: number of bootstraps, seed, and confidence interval
---
+++ conf_auc.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+import numpy as np
+import sklearn
+from sklearn import metrics
+
+
+def conf_auc(test_predictions, ground_truth, bootstrap=1000, seed=42,  confint=0.95):
+    """Takes as input test predictions, ground truth, number of bootstraps, seed, and confidence interval"""
+    #inspired by https://stackoverflow.com/questions/19124239/scikit-learn-roc-curve-with-confidence-intervals
+    bootstrapped_scores = []
+    rng = np.random.RandomState(seed)
+    for i in range(bootstrap):
+        # bootstrap by sampling with replacement on the prediction indices
+        indices = rng.randint(0, len(test_predictions) - 1, len(test_predictions))
+        if len(np.unique(ground_truth[indices])) < 2:
+            continue
+
+        score = metrics.roc_auc_score(ground_truth[indices], test_predictions[indices])
+        bootstrapped_scores.append(score)
+    
+    sorted_scores = np.array(bootstrapped_scores)
+    sorted_scores.sort()
+
+    lower_bound=(1-confint)/2
+    upper_bound=1-lower_bound
+    confidence_lower = sorted_scores[int(lower_bound * len(sorted_scores))]
+    confidence_upper = sorted_scores[int(upper_bound * len(sorted_scores))]
+    print("{:0.0f}% confidence interval for the score: [{:0.3f} - {:0.3}]".format(confint*100, confidence_lower, confidence_upper))
+
